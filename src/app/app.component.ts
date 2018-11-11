77ae5b5846ab72f18815ea13a55ba667a00511e3
JoseAlejandro <josesa10_10@hotmail.com>
2018-11-11 13:09:02 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  messageText: Array<string> = ['',''];
+  messageImage: Array<any> = ['',''];
+  messages: Array<any>;
+  users: Array<any> = [{id:1}, {id:2}];
+  public selectedFile: FileList;
+  public selectedFile2: FileList;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.messages = new Array();
+  }
+
+  sendMessage(i) {
+  	if(this.messageText[i] || this.messageImage[i]) {
+	    const message = {
+		  sender: this.users[i].id,
+	      text: this.messageText[i],
+	      image: this.messageImage[i]
+	    };
+	    console.log(message);
+	    this.messages.push(message);
+	    this.messageText[i] = '';
+	    this.messageImage[i] = '';
+	    document.getElementById("imageselect"+i).textContent = '';
+	}
+  }
+
+  addUser() {
+  	this.users.push({id:this.users.length+1})
+  	this.messageText.push('');
+  }
+
+  public getBase64(file, i) {
+    var reader = new FileReader();
+    var obj = this;
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      obj.messageImage[i] = reader.result;
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+  }
+
+  public selectFile(event,i) {
+    this.selectedFile = event.target.files;
+    var file  = event.target.files[0];
+    var reader = new FileReader();
+    if (file) {
+      console.log(file);
+      this.getBase64(file, i);
+    }
+  }
 }
